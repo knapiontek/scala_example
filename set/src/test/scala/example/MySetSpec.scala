267660a7795be5eade167cfab09975c2dd28a230
Krzysztof Napiontek <knapiontek@gmail.com>
2019-02-05 14:09:45 +0000

    add MySet with test

--- set/src/test/scala/example/MySetSpec.scala
+++ set/src/test/scala/example/MySetSpec.scala
@@ -1,8 +1,11 @@
-
 package example
 
-object MySetSpec extends App {
-	println("Hello")
-	val s = MySet(1,2,3,4)
-	s + 5 ++ MySet(-1, -2) + 3 flatMap (x => MySet(x, 10 * x)) filter (_ % 2 == 0) foreach println
+import org.scalatest._
+
+class MySetSpec extends FlatSpec with Matchers {
+	"The MySet" should "return some values" in {
+		println("Hello")
+		val s = MySet(1,2,3,4)
+		s + 5 ++ MySet(-1, -2) + 3 flatMap (x => MySet(x, 10 * x)) filter (_ % 2 == 0) foreach println
+	}
 }
