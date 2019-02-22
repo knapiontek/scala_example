80ae3211bb5af4647469df6595185ae3b0182f25
Krzysztof Napiontek <knapiontek@gmail.com>
2019-02-22 15:27:10 +0000

    for comprehansion with futures

--- udemy/src/main/scala/example/Promise.scala
+++ udemy/src/main/scala/example/Promise.scala
@@ -1,5 +1,40 @@
 package example
 
-class Promise {
+import scala.util.Random
+import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
 
+object Promise extends App {
+
+	case class Profile(id: Int, name: String) {
+		def send(other: Profile): Unit =
+			println(s"$name sends message to ${other.name}")
+	}
+
+	object Network {
+		val nodes = Map (
+			1 -> "John",
+			2 -> "Elvis",
+			3 -> "Nobody"
+		)
+
+		val friends = Map (
+			1 -> 2,
+			2 -> 3
+		)
+
+		val random = new Random()
+
+		def fetchProfile(id: Int): Future[Profile] = Future {
+			println(s"fetching profile: $id")
+			Thread.sleep(random.nextInt(300))
+			Profile(id, nodes(id))
+		}
+		def fetchFriend(profile: Profile): Future[Profile] = Future {
+			println(s"fetching friend of: ${profile.name}")
+			Thread.sleep(random.nextInt(300))
+			val friendId = friends(profile.id)
+			Profile(friendId, nodes(friendId))
+		}
+	}
 }
--- udemy/src/test/scala/example/PromiseSpec.scala
+++ udemy/src/test/scala/example/PromiseSpec.scala
@@ -1,9 +1,34 @@
 package example
 
+import example.Promise.Network
 import org.scalatest._
 
+import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
+import scala.concurrent.duration._
+
 class PromiseSpec extends FlatSpec with Matchers {
-  "The Hello object" should "say hello" in {
-    Hello.greeting shouldEqual "hello"
+  "The future object" should "send a message" in {
+    val future = Future {
+      Thread.sleep(100)
+      println("done")
+    }
+
+    future.onComplete {
+      case Success(func) => println(s"func: $func")
+      case Failure(exception) => println(s"exc: $exception")
+    }
+
+    Await.result(future, 2.seconds)
+  }
+
+  "The network" should "work" in {
+    for {
+      john <- Network.fetchProfile(1).fallbackTo(Network.fetchProfile(3))
+      elvis <- Network.fetchFriend(john)
+    } john.send(elvis)
+
+    Thread.sleep(1000)
   }
 }
